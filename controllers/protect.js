fd5147d025e778d26951328873de4eff56aa3ba8
Loordhu Jeyakumar <loordhujeyakumar@gmail.com>
2024-11-25 14:43:38 +0530

    feat(auth): Implement middleware for user authentication using JWT with cookie-based authorization

---
+++ controllers/protect.js
@@ -0,0 +1,35 @@
+import util from "util";
+import jwt from "jsonwebtoken";
+import User from "../models/userModel.js";
+import AppError from "../utils/AppError.js";
+import catchAsync from "../utils/catchAsync.js";
+
+export const protect = catchAsync(async (req, res, next) => {
+  console.log("cookie: ", req.headers.cookie);
+
+  if (!req.headers.cookie) {
+    return next(new AppError("You have to login", 404));
+  }
+
+  const cookieString = req.headers.cookie;
+  if (!cookieString) {
+    return new AppError("You have to login", 404);
+  }
+
+  const token = cookieString.split("=")[1];
+
+  const verifyAsync = util.promisify(jwt.verify);
+  const verifiedJWT = await verifyAsync(token, process.env.JWT_SECRET);
+  const currentUser = await User.findById(verifiedJWT.id).select("-__v");
+
+  if (!currentUser) {
+    // clear the cookie
+    res.clearCookie("jwt");
+    return next(
+      new AppError("the user belonging to this token does no longer exist", 401)
+    );
+  }
+  req.user = currentUser._id;
+  req.user.isAdmin = currentUser.role;
+  next();
+});
