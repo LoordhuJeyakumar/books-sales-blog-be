8fcf9a2191ebbbe0af594ad8dd420d64c276ac62
Loordhu Jeyakumar <loordhujeyakumar@gmail.com>
2024-11-25 13:56:54 +0530

    feat(database): Simplify database connection configuration

---
+++ server.js
@@ -0,0 +1,26 @@
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import app from "./app.js";
+
+dotenv.config({ path: "./.env" });
+const port = process.env.PORT || 3333;
+
+/* const DB = process.env.DATABASE.replace(
+  '<password>',
+  process.env.DATABASE_PASSWORD
+) */
+
+const DB = process.env.DATABASE;
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log(`connected to db `))
+  .catch((e) => console.log(e));
+
+app.listen(port, () => {
+  console.log(`Express server is running at http://localhost:${port}`);
+});
+
+
