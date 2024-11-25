83790bef8365976856626b758010b6f4378cd03d
Loordhu Jeyakumar <loordhujeyakumar@gmail.com>
2024-11-25 16:42:47 +0530

    git dotenv path chnaged

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import app from "./app.js";
 
-dotenv.config();
+//dotenv.config();
 const port = process.env.PORT || 3333;
 
 /* const DB = process.env.DATABASE.replace(
@@ -11,6 +11,7 @@ const port = process.env.PORT || 3333;
 ) */
 
 const DB = process.env.DATABASE;
+
 
 mongoose
   .connect(DB, {
